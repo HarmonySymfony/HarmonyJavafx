9d2d4be37ab39b689709044ba0a45f1f0dcebe89
Raed Amri <raed.amri@esprit.tn>
2024-04-24 15:26:31 +0100

    fixed

---
+++ src/main/java/controllers/EventDetailController.java
@@ -0,0 +1,18 @@
+package controllers;
+
+import entities.Evenement;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class EventDetailController {
+    @FXML
+    private Label nameLabel, descriptionLabel, priceLabel, placesLabel, addressLabel;
+
+    public void setEvent(Evenement event) {
+        nameLabel.setText("Name: " + event.getNom());
+        descriptionLabel.setText("Description: " + event.getDescription());
+        priceLabel.setText("Price: " + event.getPrix() + " $");
+        placesLabel.setText("Available Places: " + event.getPlaceDispo());
+        addressLabel.setText("Address: " + event.getAdresse());
+    }
+}
---
+++ src/main/java/interfaces/IServices.java
@@ -0,0 +1,27 @@
+package interfaces;
+
+import controllers.Personne;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface IServices <T>{
+
+
+    void Ajouter(entities.Personne personne);
+
+    void Ajouter(Personne personne);
+
+    void addEntity (T t);
+    void updateEntity(T t);
+    void deleteEntity(T t);
+    List<T> getAllData();
+
+
+
+
+
+
+
+}
+
