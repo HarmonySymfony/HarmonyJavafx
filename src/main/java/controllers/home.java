f76335f4a9be1cf8cd0724492b4e526f3b9f2d8a
Hedy <Hedy.helioui@esprit.tn>
2024-04-29 22:41:20 +0200

    Working on map

--- src/main/java/controllers/home.java
+++ src/main/java/controllers/home.java
@@ -21,7 +21,7 @@ public class Home extends Application {
     @Override
     public void start(Stage primaryStage) {
 
-            FXMLLoader loader=new FXMLLoader(getClass().getResource("/IndexPharmacie.fxml"));
+            FXMLLoader loader=new FXMLLoader(getClass().getResource("/Carte.fxml"));
         try {
             Parent root=loader.load();
             Scene scene =new Scene(root);
